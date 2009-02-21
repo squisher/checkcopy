31fe6e967c914930fed2f04683109b1c1fc61d9f
David Mohr <david@mcbf.net>
2009-02-20 22:35:59 -0700

    Fixing GTK and mhash in configure.ac, fixing mash compilation with autotools

---
+++ src/mhash-fix.h
@@ -0,0 +1,35 @@
+/* $Id: thread-copy.h 50 2009-02-16 07:36:08Z squisher $ */
+/*
+ *  Copyright (c) 2008-2009 David Mohr <david@mcbf.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __MHASH_FIX_H__
+#define __MHASH_FIX_H__
+
+/* See README.
+ * mhash's packaging is crap, and it includes its config.h.
+ * Here we undo that damage.
+ */
+
+#undef PACKAGE
+#undef PACKAGE_BUGREPORT
+#undef PACKAGE_NAME
+#undef PACKAGE_STRING
+#undef PACKAGE_TARNAME
+#undef PACKAGE_VERSION
+
+#endif // __MHASH_FIX_H__
